519d79fb103e677ce567d5192ad975e27defa569
mio-dokuhaki <hatsunemikusky521@gmail.com>
2024-02-04 00:12:34 +0900

    Update plugins.lua

--- plugins.lua
+++ plugins.lua
@@ -234,6 +234,7 @@ return packer.startup(function(use)
       },
   }
   use({ "David-Kunz/markid" })
+  use({ "wakatime/vim-wakatime" })
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
