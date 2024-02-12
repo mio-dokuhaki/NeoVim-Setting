67790b4077aaa96eb1971d9229f764840ea5aab7
mio-dokuhaki <hatsunemikusky521@gmail.com>
2024-02-12 22:03:53 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -325,19 +325,21 @@ require'lspconfig'.julials.setup{
     on_new_config = function(new_config, _)
         local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
         if require'lspconfig'.util.path.is_file(julia) then
-            vim.notify("Hello!")
+	    vim.notify("Hello!")
             new_config.cmd[1] = julia
         end
     end
 }
 
 require('treepin').setup {
-        hide_onscreen = true, -- Hide's the pin buffer when the text of the pin is visible.
-        max_height = 30, -- Prevents the pin buffer from displaying when the pin is larger than x lines.
-        position = 'relative', -- May be 'relative', 'top', or 'bottom'. Determines the position of the pin buffer within the window.
-        icon = '>', -- The icon to display in the sign column at the top of the pin. Set to nil to prevent the sign column being used.
-        zindex = 50, -- The Z-index of the pin buffer.
-        separator = nil, -- A single character that may be used as a separator between the editing buffer and the pin buffer.
+	hide_onscreen = true, -- Hide's the pin buffer when the text of the pin is visible.
+	max_height = 30, -- Prevents the pin buffer from displaying when the pin is larger than x lines.
+	position = 'relative', -- May be 'relative', 'top', or 'bottom'. Determines the position of the pin buffer within the window.
+	icon = '>', -- The icon to display in the sign column at the top of the pin. Set to nil to prevent the sign column being used.
+	zindex = 50, -- The Z-index of the pin buffer.
+	separator = nil, -- A single character that may be used as a separator between the editing buffer and the pin buffer.
 }
 
 require("flutter-tools").setup {} -- use defaults
+
+require('trim').setup()
