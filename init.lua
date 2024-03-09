9e39e7b2065aefcfbc277f3a8a5ce6b99fd621ca
mio-dokuhaki <hatsunemikusky521@gmail.com>
2024-03-09 15:44:04 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -209,7 +209,7 @@ require('nvim-navic').setup {
 require("solarized-osaka").setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    transparent = false,    -- Enable this to disable setting the background color
+    transparent = true,     -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
     styles = {
         -- Style to be applied to different syntax groups
@@ -225,18 +225,18 @@ require("solarized-osaka").setup({
     sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
     day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-    dim_inactive = false,             -- dims inactive windows
-    lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
+    dim_inactive = true,              -- dims inactive windows
+    lualine_bold = true,              -- When `true`, section headers in the lualine theme will be bold
 
     --- You can override specific color groups to use other groups or a hex color
     --- function will be called with a ColorScheme table
-    ---@param colors ColorScheme
+    --@param colors ColorScheme
     on_colors = function(colors) end,
 
     --- You can override specific highlights to use other groups or a hex color
     --- function will be called with a Highlights and ColorScheme table
-    ---@param highlights Highlights
-    ---@param colors ColorScheme
+    --@param highlights Highlights
+    --@param colors ColorScheme
     on_highlights = function(highlights, colors) end,
 })
 
@@ -349,6 +349,10 @@ lspconfig.sumneko_lua.setup({
         Lua = {
             hint = {
                 enable = true,
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
             },
         },
     },
