257ff287f88fdfaa6fa01dd0406f4be8a90b3315
mio-dokuhaki <hatsunemikusky521@gmail.com>
2024-04-23 23:39:37 +0900

    Update autocmds.lua

--- autocmds.lua
+++ autocmds.lua
@@ -3,20 +3,31 @@ local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 -- Remove whitespace on save
 autocmd("BufWritePre", {
-	pattern = "*",
-	command = ":%s/\\s\\+$//e",
+    pattern = "*",
+    command = ":%s/\\s\\+$//e",
 })
 
 -- Don't auto commenting new lines
 autocmd("BufEnter", {
-	pattern = "*",
-	command = "set fo-=c fo-=r fo-=o",
+    pattern = "*",
+    command = "set fo-=c fo-=r fo-=o",
 })
 
 -- Restore cursor location when file is opened
 autocmd({ "BufReadPost" }, {
-	pattern = { "*" },
-	callback = function()
-		vim.api.nvim_exec('silent! normal! g`"zv', false)
-	end,
+    pattern = { "*" },
+    callback = function()
+        vim.api.nvim_exec('silent! normal! g`"zv', false)
+    end,
+})
+
+-- JavaScript と TypeScript ファイル用の設定
+autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "*.js", "*.ts" },
+    callback = function()
+        -- インデントを2スペースに設定
+        vim.bo.tabstop = 2
+        vim.bo.shiftwidth = 2
+        vim.bo.expandtab = true
+    end,
 })
