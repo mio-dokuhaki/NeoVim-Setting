16c9775f391e7c2c455dd8e627bff123a2dcf4b8
mio-dokuhaki <hatsunemikusky521@gmail.com>
2024-05-02 19:51:55 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -668,3 +668,21 @@ require 'lspconfig.configs'.lsp_wl = {
 }
 
 nvim_lsp.lsp_wl.setup({ on_attach = lsp_attach({}) })
+
+require 'FTerm'.setup {}
+
+local fterm = require("FTerm")
+
+local gitui = fterm:new({
+    ft = 'fterm_gitui', -- You can also override the default filetype, if you want
+    cmd = "gitui",
+    dimensions = {
+        height = 0.9,
+        width = 0.9
+    }
+})
+
+-- Use this to toggle gitui in a floating terminal
+vim.keymap.set('n', '<A-g>', function()
+    gitui:toggle()
+end)
