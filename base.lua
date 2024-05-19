b88e2c2759a6713ee59d089d67036bd1aeeaa543
mio-dokuhaki <hatsunemikusky521@gmail.com>
2024-05-19 17:00:01 +0900

    Update base.lua

--- base.lua
+++ base.lua
@@ -8,19 +8,3 @@ vim.wo.number = true
 vim.api.nvim_create_user_command("Hoge", function(opts)
     vim.cmd("e " .. "~/_/hoge/hoge.markdown")
 end, {})
-
-vim.api.nvim_create_user_command('FTermOpen', require('FTerm').open, { bang = true })
-
-vim.api.nvim_create_user_command('FTermClose', require('FTerm').close, { bang = true })
-
-vim.api.nvim_create_user_command('FTermExit', require('FTerm').exit, { bang = true })
-
-vim.api.nvim_create_user_command('FTermToggle', require('FTerm').toggle, { bang = true })
-
-vim.api.nvim_create_user_command('ManLs', function()
-    require('FTerm').run('man ls')
-end, { bang = true })
-
-vim.api.nvim_create_user_command('YarnBuild', function()
-    require('FTerm').run({'yarn', 'build'})
-end, { bang = true })
